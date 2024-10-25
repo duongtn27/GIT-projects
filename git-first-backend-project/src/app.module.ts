5d43ea0e76fe84ebe1a0ae87a313a1932a080350
Phi An Binh <phianbinh2001@gmail.com>
2024-10-25 14:46:22 +0700

    created modules: history, notification

--- git-first-backend-project/src/app.module.ts
+++ git-first-backend-project/src/app.module.ts
@@ -7,35 +7,41 @@ import { Post } from './posts/entities/post.entity';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
+import { HistoriesModule } from './histories/histories.module';
+import { History } from './histories/entities/histories.entity';
+import { NotificationsModule } from './notifications/notifications.module';
+import { Notification } from './notifications/entities/notifications.entity';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      load: [configuration],
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USER'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        entities: [Post, User],
-        synchronize: true,
-        ssl: {
-          rejectUnauthorized: false,
-        }
-      }),
-    }),
-    PostsModule,
-    DatabaseModule,
-    UserModule,
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            load: [configuration],
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get<string>('DATABASE_HOST'),
+                port: configService.get<number>('DATABASE_PORT'),
+                username: configService.get<string>('DATABASE_USER'),
+                password: configService.get<string>('DATABASE_PASSWORD'),
+                database: configService.get<string>('DATABASE_NAME'),
+                entities: [History, Notification],
+                synchronize: true,
+                ssl: {
+                    rejectUnauthorized: false,
+                }
+            }),
+        }),
+        PostsModule,
+        DatabaseModule,
+        UserModule,
+        HistoriesModule,
+        NotificationsModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule { }
