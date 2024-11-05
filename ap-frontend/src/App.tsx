bd16b308bb0ae5c38e06f08767fab0dd428dbf33
duongtn27 <tr.ngocduong01@gmail.com>
2024-11-05 20:20:58 +0700

    demo login component

--- ap-frontend/src/App.tsx
+++ ap-frontend/src/App.tsx
@@ -1,20 +1,58 @@
-// // import {Header, Footer} from '../components/header.tsx'
-// // import Footer from '../components/footer.tsx'
-// import S from '../components/header.tsx'
-import Header from '../components/header.tsx'
-import Footer from '../components/footer.tsx'
+import { Button, Form, Input } from "antd"
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import Cookies from 'js-cookie';
 
 function App() {
-  const name = "LMAO";
-  const catName = ["1","2","3"];
+  const onFinish = async (values: any) => {
+    try {
+      const response = await fetch('http://localhost:3001/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: values.username,
+          password: values.password,
+        }),
+      });
+
+      const data = await response.json();
+
+      Cookies.set('access_token', data.data.access_token);
+
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <>
-      {/* <S.Header /> 
-      <S.Footer /> */}
-      <Header catname = {catName} />
-      <h1>Header HELLO {name} </h1>
-      <Footer />
-      <h1>Footer HELLO {name} </h1>
+      <Form
+        name="login"
+        initialValues={{ remember: true }}
+        style={{ maxWidth: 360 }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
+        </Form.Item>
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+
     </>
   )
 }
