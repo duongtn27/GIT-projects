809f98574b4ab50af679509238da1c17c0c04857
JaNuArY-17th <nhl170100@gmail.com>
2024-12-27 19:39:21 +0700

    3

--- git-first-backend-project/src/room/entities/room.entity.ts
+++ git-first-backend-project/src/room/entities/room.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, IsNull, Generated, PrimaryColumn, ManyToOne } from "typeorm";
-import { Campus } from '../../campus/entities/campus.entity'
-import { Schedule } from '../../schedule/entities/schedule.entity'
+import { Campus } from '../../campus/entities/campus.entity';
+import { Schedule } from '../../schedule/entities/schedule.entity';
+
 @Entity("rooms")
 export class Room {
     @PrimaryColumn({ type: 'uuid' })
@@ -19,7 +20,7 @@ export class Room {
     @Column({ nullable: true })
     campus_id: string;
 
-    @ManyToOne(() => Campus, campus => campus.rooms) 
+    @ManyToOne(() => Campus, campus => campus.id) 
     campus: Campus;
 
     @ManyToOne(() => Schedule, schedule => schedule.rooms) 
