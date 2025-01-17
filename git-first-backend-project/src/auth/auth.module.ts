97c01d481303ad6b61cdf95b208631687c509995
Phi An Binh <phianbinh2001@gmail.com>
2025-01-17 19:33:16 +0700

    implement things

--- git-first-backend-project/src/auth/auth.module.ts
+++ git-first-backend-project/src/auth/auth.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+    imports: [
+        UserModule,
+        PassportModule,
+        ConfigModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get('JWT_SECRET'),
+                signOptions: {
+                    expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
+                },
+            }),
+        }),
+    ],
+    providers: [AuthService, LocalStrategy],
+    controllers: [AuthController]
 })
-export class AuthModule {}
+export class AuthenticationModule { }
--- git-first-backend-project/src/auth/auth.service.ts
+++ git-first-backend-project/src/auth/auth.service.ts
@@ -1,50 +1,65 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { HttpException, HttpStatus } from '@nestjs/common';
-import { RegisterDto } from './dto/register.dto'
+import { RegisterDto } from './dto/register.dto';
+import { JwtService } from '@nestjs/jwt';
+import TokenPayload from './tokenPayload.interface';
+import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    // constructor(
-    //     private readonly usersService: UserService
-    // ) { }
+    constructor(
+        private readonly usersService: UserService,
+        private readonly jwtService: JwtService,
+        private readonly configService: ConfigService
+    ) { }
 
-    // public async register(registrationData: RegisterDto) {
-    //     const hashedPassword = await bcrypt.hash(registrationData.password, 10);
-    //     try {
-    //         const createdUser = await this.usersService.create({
-    //             ...registrationData,
-    //             password: hashedPassword
-    //         });
-    //         createdUser.password = undefined;
-    //         return createdUser;
-    //     } catch (error) {
-    //         // if (error?.code === PostgresErrorCode.UniqueViolation) {
-    //         //     throw new HttpException('User with that email already exists', HttpStatus.BAD_REQUEST);
-    //         // }
-    //         throw new HttpException('Something went wrong', HttpStatus.INTERNAL_SERVER_ERROR);
-    //     }
-    // }
+    public async register(registrationData: RegisterDto) {
+        const hashedPassword = await bcrypt.hash(registrationData.password, 10);
+        try {
+            const createdUser = await this.usersService.create({
+                ...registrationData,
+                password: hashedPassword
+            });
+            createdUser.password = undefined;
+            return createdUser;
+        } catch (error) {
+            // if (error?.code === PostgresErrorCode.UniqueViolation) {
+            //     throw new HttpException('User with that email already exists', HttpStatus.BAD_REQUEST);
+            // }
+            throw new HttpException('Something went wrong', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
-    // public async getAuthenticatedUser(email: string, plainTextPassword: string) {
-    //     try {
-    //         const user = await this.usersService.getByEmail(email);
-    //         await this.verifyPassword(plainTextPassword, String(user.password));
-    //         user.password = undefined;
-    //         return user;
-    //     } catch (error) {
-    //         throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
-    //     }
-    // }
+    public getCookieWithJwtToken(userId: number) {
+        const payload: TokenPayload = { userId };
+        const token = this.jwtService.sign(payload);
+        return `Authentication=${token}; HttpOnly; Path=/; Max-Age=${this.configService.get('JWT_EXPIRATION_TIME')}`;
+    }
 
-    // private async verifyPassword(plainTextPassword: string, hashedPassword: string) {
-    //     const isPasswordMatching = await bcrypt.compare(
-    //         plainTextPassword,
-    //         hashedPassword
-    //     );
-    //     if (!isPasswordMatching) {
-    //         throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
-    //     }
-    // }
+    public getCookieForLogOut() {
+        return `Authentication=; HttpOnly; Path=/; Max-Age=0`;
+    }
+
+    public async getAuthenticatedUser(email: string, plainTextPassword: string) {
+        try {
+            const user = await this.usersService.getByEmail(email);
+            await this.verifyPassword(plainTextPassword, String(user.password));
+            user.password = undefined;
+            return user;
+        } catch (error) {
+            throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private async verifyPassword(plainTextPassword: string, hashedPassword: string) {
+        const isPasswordMatching = await bcrypt.compare(
+            plainTextPassword,
+            hashedPassword
+        );
+        if (!isPasswordMatching) {
+            throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
+        }
+    }
 }
